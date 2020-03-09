b419ae030d7103ec3366fefd710d62d6afabe2a4
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-09 00:31:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,9 +28,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  array.find do |item|
-    cheese_types.include?
-    (item)
+    array.find do |type|
+    cheese_types.include?(type)
   end
 end
